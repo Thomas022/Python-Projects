33daed0b56389717bb8b401ef875e5e2a5c6fc39
Thomas Musieracki <64069553+Thomas022@users.noreply.github.com>
2026-09-10 16:19:39 -0300

    Update Gradebook_Project.py

--- Gradebook_Project.py
+++ Gradebook_Project.py
@@ -1,5 +1,5 @@
-Gradebook
-You are a student and you are trying to organize your subjects and grades using Python. Let’s explore what we’ve learned about lists to organize your subjects and scores.
+#Gradebook
+#You are a student and you are trying to organize your subjects and grades using Python. Let’s explore what we’ve learned about lists to organize your subjects and scores.
 
 last_semester_gradebook = [["politics", 80], ["latin", 96], ["dance", 97], ["architecture", 65]]
 
